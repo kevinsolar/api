9c6ceb9e86cb281e0dea66e834a0c91d60a15ce5
Kevin Solar <kevinleitesolar@hotmail.com>
2025-07-03 17:01:24 -0300

    making methods and propertys prived

--- src/database.js
+++ src/database.js
@@ -3,37 +3,36 @@ import fs from "node:fs/promises"
 const DATABASE_PATH = new URL("db.json", import.meta.url)
 
 export class Database {
-	database = {}
+	#database = {}
 
 	constructor() {
 		fs.readFile(DATABASE_PATH, "utf8")
 			.then((data) => {
-				this.database = JSON.parse(data)
+				this.#database = JSON.parse(data)
 			})
-			.catch(() => this.persist())
-		this.persist()
+			.catch(() => this.#persist())
 	}
 
-	persist() {
-		fs.writeFile(DATABASE_PATH, JSON.stringify(this.database))
+	#persist() {
+		fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database))
 	}
 
 	insert(table, data) {
 		/*
       O que estou tentando fazer aqui eh, verificando dentro da minha colecao se existe essa tabela.
     */
-		if (Array.isArray(this.database[table])) {
-			this.database[table].push(data)
+		if (Array.isArray(this.#database[table])) {
+			this.#database[table].push(data)
 		} else {
 			// se nao existir, entao eu crio uma nova tabela passando o dado
-			this.database[table] = [data]
+			this.#database[table] = [data]
 		}
 
 		// passa os dados de database para o DATABASE
-		this.persist()
+		this.#persist()
 	}
 
 	select(table) {
-		return this.database[table]
+		return this.#database[table] ?? []
 	}
 }
